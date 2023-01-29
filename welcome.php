8646fc3fc66a4d73758224f37169d14866c97998
katherynisabell <111913185+katherynisabell@users.noreply.github.com>
2023-01-29 17:57:01 -0600

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>  
+    <body>
+
+        <form action="welcome.php" method="post">
+            Name: <input type="text" name="name"><br>
+            How many times do you want to see your name? <input type="int" num="num"><br>
+        </form>
+    </body>
+</html> 
